14c6e6fa1afb5996bac077f06f9934e01234461c
MARk3108 <mark.fomin.2003@gmail.com>
2024-01-20 21:33:25 +0300

    Gorutines

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
+
+var mu sync.Mutex
 
 func getPort() string {
 	port := os.Getenv("PORT")
@@ -19,12 +23,45 @@ func getPort() string {
 	return port
 }
 
+func readFromFile() (string, error) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	content, err := ioutil.ReadFile("cur.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return "", err
+	}
+
+	return string(content), nil
+}
+
+func writeToFile(body string) error {
+	mu.Lock()
+	defer mu.Unlock()
+
+	file, err := os.OpenFile("cur.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return err
+	}
+	defer file.Close()
+
+	// Write data to the file
+	if _, err := file.WriteString(body + "\n"); err != nil {
+		fmt.Println("Error writing to file:", err)
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"message": "Hello, Railway!",
+			"message": "Main page",
 		})
 	})
 
@@ -34,19 +71,13 @@ func main() {
 		// Выводим тело запроса в консоль
 		fmt.Println("Received request body:", body)
 
-		file, err := os.OpenFile("cur.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		err := writeToFile(string(body))
 		if err != nil {
-			fmt.Println("Error opening file:", err)
-		}
-		defer file.Close()
-
-		// Записываем данные в файл
-		if _, err := file.WriteString(string(body) + "\n"); err != nil {
-			fmt.Println("Error writing to file:", err)
+			return c.Status(fiber.StatusInternalServerError).SendString("Error writing to file")
 		}
 
-		// Возвращаем сообщение клиенту
-		return c.SendString("Data received successfully")
+		// Return a success message to the client
+		return c.SendString("Data received and written to file successfully")
 	})
 
 	app.Get("/read", func(c *fiber.Ctx) error {
@@ -56,5 +87,20 @@ func main() {
 		}
 		return c.JSON(fiber.Map{"massage": string(content)})
 	})
+
+	go func() {
+		for {
+			content, err := readFromFile()
+			if err != nil {
+				fmt.Println("Error reading from file:", err)
+			} else {
+				fmt.Println("File content:", content)
+			}
+
+			// Sleep for one second
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
 	app.Listen(getPort())
 }
